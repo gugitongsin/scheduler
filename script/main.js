ee220b750e117e0b8fefc0eb2f4d6bd3ca087f22
jskee <gugitongsin@gmail.com>
2020-09-17 16:13:05 +0900

    0917

--- script/main.js
+++ script/main.js
@@ -1,7 +1,114 @@
-$(function(){
+//전역상수, 변수
+const d = new Date();
+const YEAR = d.getFullYear();
+const MONTH = d.getMonth();
+const DATE = d.getDate();
+const DAY = d.getDay();
+const YEARMONTH = ""+YEAR+addZero(MONTH+1);
+var id = "";
+var url = "";
+var targetDate = "";
 
-    timer();
+
+//onload entry
+$(function(){
+    init_timer();
+    init_calender();
+    
+    //event
+    $('.btn_find').on('click', function(){
+        $('.box_date').addClass('off')
+        $('.box_calender').addClass('on')
+        id = $('.ipt_id').val();
+        url = `../json/${YEARMONTH+id}.json`
+        httpGet(url, data => {
+            renderData(data);
+        }, error => alert("등록되지 않은 사용자") );
+    })
+
+    $('a.col').on('click', function(){
+        targetDate = $(this).find('span.date').text();
+        if( targetDate.length > 0 ) $('.box_detail').addClass('on');
+        httpGet(url, data => {
+            var obj = JSON.parse(data);
+            const wHour = obj[targetDate].W;
+            const lHour = obj[targetDate].L;
+            const cHour = obj[targetDate].C;
+            const wContent = obj[targetDate].W_Content.split(",");
+            const lContent = obj[targetDate].L_Content.split(",");
+            const cContent = obj[targetDate].C_Content.split(",");
+            $('.t_work .txt_total').text(wHour);
+            $('.t_life .txt_total').text(lHour);
+            $('.t_career .txt_total').text(cHour);
+            
+            for(var i=0; i<wContent.length; i++){
+                $('.c_work .list_content').append(`
+                    <li>
+                        <span class="content">${wContent[i]}</span>
+                    </li>
+                `)
+            }
+            for(var i=0; i<lContent.length; i++){
+                $('.c_life .list_content').append(`
+                    <li>
+                        <span class="content">${lContent[i]}</span>
+                    </li>
+                `)
+            }
+            for(var i=0; i<cContent.length; i++){
+                $('.c_career .list_content').append(`
+                    <li>
+                        <span class="content">${cContent[i]}</span>
+                    </li>
+                `)
+            }
+        }, error => alert("사용자 정보를 불러올 수 없습니다.") );
+    })
+
+    $('.btn_more').on('click', function(){
+        $('.box_detail > div').removeClass('on');
+        var idx = $(this).parent().parent().index();
+        $('.box_detail > div').eq(idx).addClass('on');
+    })
+    $('.btn_save').on('click', function(){
+        
+    })
 })
+
+
+
+// function def
+var init_calender = function(){
+    var first = new Date(YEAR, MONTH, 1);
+    var dayFirst = first.getDay();
+    var count = 1;
+    const initialColumn = 7;
+
+    while( count <= 31 ){
+        $('.col').eq(initialColumn + dayFirst + count).prepend(`
+        <span class="date">${count}</span>
+        <div class="box_content">
+            <ul>
+                <li>
+                    <span class="symbol s_work">W</span>
+                    <span class="value v_work"></span>
+                </li>
+                <li>
+                    <span class="symbol s_life">L</span>
+                    <span class="value v_life"></span>
+                </li>
+                <li>
+                    <span class="symbol s_career">C</span>
+                    <span class="value v_career"></span>
+                </li>
+            </ul>
+            <span class="notice">o</span>
+        </div>
+    `)
+        count++;
+    } 
+    
+}
 
 function addZero(n){
     if(n <= 9) n = '0'+n;
@@ -17,27 +124,32 @@ function printDate(str1, str2){
     `);
 }
 
-function timer(){
-    var h = setTimeout(function(){
+//시간 출력을 위한 함수
+function init_timer(){
+    var date = new Date();
+    var yyyy = date.getFullYear();
+    var mm = date.getMonth() + 1;
+    var dd = date.getDate();
+    var ss = date.getSeconds();
+    var hh = date.getHours();
+    var min = date.getMinutes();
 
-        var date = new Date();
-        var yyyy = date.getFullYear();
-        var mm = date.getMonth() + 1;
-        var dd = date.getDate();
-        var ss = date.getSeconds();
-        var hh = date.getHours();
-        var min = date.getMinutes();
-    
-        mm = addZero(mm);
-        dd = addZero(dd);
-        ss = addZero(ss);
-        min = addZero(min);
+    mm = addZero(mm);
+    dd = addZero(dd);
+    ss = addZero(ss);
+    min = addZero(min);
 
-        printDate(yyyy+'년 '+ mm+'월 '+ dd+'일 ', hh+':'+min+':'+ss);
-        timer();
-    }, 1000);
+    printDate(yyyy+'년 '+ mm+'월 '+ dd+'일 ', hh+':'+min+':'+ss);
+    timer();
 }
 
+//settimeout
+function timer(){
+    var h = setTimeout(function(){
+        init_timer();
+    }, 1000);
+}
+// 사용자 data 요청 ajax
 function httpGet(url, successCallback, errorCallback){
     const req = new XMLHttpRequest();
     req.onload = () =>{
@@ -52,7 +164,30 @@ function httpGet(url, successCallback, errorCallback){
     req.setRequestHeader('Accept', 'application/json');
     req.send();
 }
+// json을 스케줄러에 rendering 하는 함수
+function renderData(d){
+    var obj = JSON.parse(d);
+    var first = 0;
+    var targetCol = 0;
+    var w="", l="", c = "";
+    //1일에 해당하는 col eq를 찾습니다.
+    $('span.date').each(function(){
+        if($(this).text() === '1'){ first = $(this).parent().index();}
+    })
+    targetCol = first;
+    //1일부터 31일까지 사용자의 data를 입력합니다.
+    for(var i=1; i<=31; i++){
+        if( i !== undefined ){
+            w = obj[i.toString()]["W"];
+            l = obj[i.toString()]["L"];
+            c = obj[i.toString()]["C"];
+            notice = obj[i.toString()]["notice"];
+            $('a.col').eq(targetCol).find('span.v_work').text(w);
+            $('a.col').eq(targetCol).find('span.v_life').text(l);
+            $('a.col').eq(targetCol).find('span.v_career').text(c);
+            $('a.col').eq(targetCol).find('span.notice').text(notice);
+            targetCol++;
+        }
+    }
+}
 
-httpGet('../css/reset.css', data => {
-    console.log(data);
-}, error => alert(error) );
